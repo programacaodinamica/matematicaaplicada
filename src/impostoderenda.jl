1dbb32a58ff6a1771902afe5ea1f4f78cab13053
Hallison Paz <hallison.dapaz@gmail.com>
2021-04-06 17:08:22 -0300

    após gravação do vídeo MAP 01

--- src/impostoderenda.jl
+++ src/impostoderenda.jl
@@ -65,7 +65,10 @@ md""" ## Perguntas
 """
 
 # ╔═╡ df553db6-50cc-4a93-a73c-195374a8751b
+13 * 1903.98
 
+# ╔═╡ ff3a1d48-6aff-4587-a7b0-6cf6bcf21367
+12 * 1903.98
 
 # ╔═╡ daa7435f-1a2a-48ed-8401-ee4d629cb29d
 md"""
@@ -75,16 +78,24 @@ md"""
 
 # ╔═╡ 451b58d8-0674-403d-b132-d528de0d9644
 begin 
-	aliquotas = [7.5, 15.0, 22.5, 27.5]
-	deducao = [142.80, 354.80, 636.13, 869.36]
-
+	aliquotas = [0.0, 7.5, 15.0, 22.5, 27.5]
+	deducao = [0.0, 142.80, 354.80, 636.13, 869.36]
+	
 	function impostoretido(rendimento)
-		0.0
+		limites = [0.0, 1903.99, 2826.66, 3751.06, 4664.69]
+		faixa = 5
+		for i in 1:length(aliquotas)-1
+			if limites[i] <= rendimento < limites[i+1]
+				faixa = i
+			end
+		end
+		
+		rendimento * aliquotas[faixa] / 100 - deducao[faixa]		
 	end
 end
 
 # ╔═╡ ed2071e8-f1b2-4030-bb55-ecf6d9e14a05
-salario = 3000.00
+salario = 5000.00
 
 # ╔═╡ d29f0d7e-a382-42e9-9191-df06ec8db07d
 md"""
@@ -93,7 +104,7 @@ Contribuição ao Instituto Nacional do Seguro Social (INSS) - previdenciária, 
 | Salário 								| Alíquota 		|
 | :-----------------------------------: | :-----------: |
 | Até R\\$ 1.100						| 7,5%			|
-| De R\\$ 1.100,01 até R\\$ 2.203,45	| 9% 			|
+| De R\\$ 1.100,01 até R\\$ 2.203,48	| 9% 			|
 | De R\\$ 2.203,49 até R\\$ 3.305,22	| 12% 			|
 | De R\\$ 3.305,23 até R\\$ 6.433,57	| 14% 			|
 
@@ -102,9 +113,18 @@ Contribuição ao Instituto Nacional do Seguro Social (INSS) - previdenciária, 
 
 # ╔═╡ 9dd8d374-18f5-4667-8348-8f00b3118b4e
 function contribINSS(rendimento)
-	limites = [0.0, 1100.00, 2203.45, 3305.22, 6433.57]
-	alqtINSS = [7.5, 9.0, 12.0, 14.0]
-	0.0
+	limites = [0.0, 1100.01, 2203.46, 3305.23, 6433.58]
+	alqtINSS = [7.5, 9.0, 12.0, 14.0] / 100
+	
+	desconto = 0.0
+	base = min(rendimento, limites[end] - 0.01)
+	for i in 1:length(limites)-1
+		if base > limites[i]
+			desconto += min(base - limites[i], 
+							limites[i+1] - limites[i]) * alqtINSS[i]
+		end
+	end
+	desconto
 end
 
 # ╔═╡ ee7a5da5-0142-4e1e-a326-5670a932abd3
@@ -173,12 +193,13 @@ end
 # ╟─5c31cb08-528d-4afd-97c4-f31e906c1f2f
 # ╟─37419e62-923b-11eb-01cd-cd06444df836
 # ╠═df553db6-50cc-4a93-a73c-195374a8751b
+# ╠═ff3a1d48-6aff-4587-a7b0-6cf6bcf21367
 # ╟─daa7435f-1a2a-48ed-8401-ee4d629cb29d
-# ╟─451b58d8-0674-403d-b132-d528de0d9644
-# ╟─ee7a5da5-0142-4e1e-a326-5670a932abd3
+# ╠═451b58d8-0674-403d-b132-d528de0d9644
 # ╠═ed2071e8-f1b2-4030-bb55-ecf6d9e14a05
 # ╟─d29f0d7e-a382-42e9-9191-df06ec8db07d
-# ╟─9dd8d374-18f5-4667-8348-8f00b3118b4e
+# ╠═9dd8d374-18f5-4667-8348-8f00b3118b4e
+# ╟─ee7a5da5-0142-4e1e-a326-5670a932abd3
 # ╟─7a173210-c864-4f8c-9c77-607b5b2b7b74
 # ╟─e59d4f5a-bee7-4708-a888-55148688c550
 # ╟─ac2c9f10-04ba-4525-b6e3-561fb928bd02
